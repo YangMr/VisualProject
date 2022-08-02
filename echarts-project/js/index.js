fba3668175f61d9ee3de523b16e183ccb9ebe7ec
cstp <759168442@qq.com>
2022-08-02 15:37:59 +0800

    feat(新功能): 实现订单模块功能以及销售额模块部分功能

--- echarts-project/js/index.js
+++ echarts-project/js/index.js
@@ -216,4 +216,167 @@
     })
 })();
 
+(function (){
+    $(document).ready(function (){
+        let data = {
+            day365 : {
+                orders: '30,321,988',
+                amount: '99882'
+            },
+            day90: {
+                orders: '301,987',
+                amount: '9834'
+            },
+            day30: {
+                orders: '1,987',
+                amount: '3834'
+            },
+            day1: {
+                orders: '987',
+                amount: '834'
+            }
+        }
+
+        const $h4Order = $(".order .data  h4:eq(0)")
+        const $h4Amount = $(".order .data  h4:eq(1)")
+        let _index = 1
+        $(".order .tabs a").click(function (){
+            _index = $(this).index()
+            tabs()
+        })
+
+
+        setInterval( ()=>{
+            tabs()
+            _index += 1
+            if(_index >= 4){
+                _index = 0
+            }
+        },1000)
+
+
+        function tabs(){
+            $(".order .tabs a").eq(_index).addClass("active").siblings().removeClass("active")
+
+            // 获取标签自定义的属性值
+            const _attr = $(".order .tabs a").get(_index).dataset.type
+            const _data = data[_attr]
+            $h4Order.html(_data.orders)
+            $h4Amount.html(_data.amount)
+        }
+    })
+})();
+
+(function (){
+    $(document).ready(function(){
+        let _index = 0
+        $(".sales .tabs ").on("click",'a',function (){
+            _index = $(this).index()
+            tabs()
+        })
+
+        setInterval(()=>{
+            tabs()
+            _index += 1
+            if(_index >= 4){
+                _index = 0
+            }
+        },1000)
+
+        function tabs(){
+            $(".sales .tabs a").eq(_index).addClass("active").siblings().removeClass("active")
+        }
+
+
+        const line = document.querySelector(".line")
+        const myChart = echarts.init(line)
+
+        const option = {
+            color : ["#00f2f1","#ed3f35"],
+            title: {
+                text: '单位: 万',
+                textStyle : {
+                    color : "#4c9bfd",
+                    fontSize : 12
+                }
+            },
+            tooltip: {
+                trigger: 'axis',
+                backgroundColor : 'rgba(0,0,0,0.3)',
+                borderWidth : 0,
+                textStyle : {
+                    color : "#fff"
+                }
+            },
+            legend: {
+                data: ['预期销售额', '实际销售额'],
+                textStyle : {
+                    color : "#4c9bfd"
+                },
+                right : "10%"
+            },
+            grid: {
+                left: '0%',
+                right: '3%',
+                bottom: '3%',
+                top : '20%',
+                containLabel: true,
+                show : true,
+                borderColor : "#012f4a"
+            },
+            xAxis: {
+                type: 'category',
+                boundaryGap: false,
+                data: ['Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat', 'Sun'],
+                axisTick: {
+                    alignWithLabel: false,
+                    show : false
+                },
+                axisLabel : {
+                    color : '#4c9bfd'
+                },
+                axisLine: {
+                    show : false
+                }
+            },
+            yAxis: {
+                type: 'value',
+                axisTick :{
+                   alignWithLabel: false,
+                   show : false
+                },
+                axisLabel : {
+                    color : '#4c9bfd'
+                },
+                axisLine: {
+                    show : false
+                },
+                splitLine : {
+                    lineStyle : {
+                        color: "#012f4a"
+                    }
+                }
+            },
+            series: [
+                {
+                    name: '预期销售额',
+                    type: 'line',
+                    data: [120, 132, 101, 134, 90, 230, 210]
+                },
+                {
+                    name: '实际销售额',
+                    type: 'line',
+                    data: [220, 182, 191, 234, 290, 330, 310]
+                }
+            ]
+        };
+
+        myChart.setOption(option)
+
+        window.addEventListener("resize", function () {
+            myChart.resize()
+        })
+    })
+})();
+
 
